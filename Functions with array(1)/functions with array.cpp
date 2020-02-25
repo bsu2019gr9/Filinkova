cfad53d5eb1b03ad1a464d9b655084de649ee164
dariafilinkova <59262546+dariafilinkova@users.noreply.github.com>
2020-02-26 00:18:24 +0300

    Update functions with array.cpp

--- Functions with array(1)/functions with array.cpp
+++ Functions with array(1)/functions with array.cpp
@@ -4,98 +4,111 @@
 #include<ctime>
 #include "FunctionsWithArrays.h"
 using namespace std;
-void initArray(int* arr, int N, int A = -10, int B = 10)//инициализация значениий элементов массива случайными числами от А до В (от -10 до 10).
+
+void* makeArrOfInt(int*& arr, int N) {
+	arr = new (nothrow) int[N];
+	if (!arr) exit(404);}
+
+/*  int* makeArrOfInt(const int N) {
+	int* arr = nullptr;
+	arr = new int[N];
+	return arr;}*/
+
+void freeMemory(int*& arr) {
+	delete[] arr;  arr = nullptr; }
+
+void initRandomArray(int* beg, int* end, const int M, const int m){
+	srand(time(0));
+	int d = M - m + 1;
+	for (int* p = beg; p < end; ++p)
+		* p = rand() % d + m;}
+
+void initArray(int* beg, int* end, const int m = 0) {
+	for (int* p = beg; p < end; ++p)
+	* p = m;}
+
+void initArray2(int* beg, int* end, int A = -10, int B = 10)//инициализация значениий элементов массива случайными числами от А до В (от -10 до 10).
 {
 	srand(time(NULL));
 	double k1 = (B - A) * 1.0 / (RAND_MAX);
-	for (int i = 0; i < N; ++i)
-		arr[i] = k1 * rand() + A;
+	for (int* p = beg; p < end; ++p)
+		* p = k1 * rand() + A;
 }
-void printArray(int* arr, const int N) //печать массива
-{for (int i = 0; i < N; ++i)cout << arr[i]<< '\n';}
 
-void inputArray(int* arr, const int N) //Ввод значений элементов массива размера N с клавиатуры
-{for (int i = 0; i < N; ++i)cin >> arr[i];}
+void printArray(int* beg, int* end){
+ for (int* p = beg; p < end; ++p)
+	cout << setw(3) << *p << ";";
+ cout << '\n';}
 
-int findMin(int* arr, const int N)//возвращает минимум в массиве 
-{
-	int min = arr[0];
-	for (int i = 0; i < N; ++i) if (arr[i] < min)min = arr[i];
+void inputArray(int* beg, int* end) //Ввод значений элементов массива размера N с клавиатуры
+{for (int* p = beg; p < end; ++p) cin >> *p; cout << '\n';}
+
+void swap(int& a, int& b){
+	int buf;
+	buf = a;a = b;b = buf;}
+
+void reverse(int* beg, int* end) {
+	for (int* p = beg, *q = end - 1; p < q; ++p, --q)
+	{swap(*p, *q);}
+}
+
+int* findMin(int* beg, int* end){
+	int*  min = beg;
+	for (int* p = beg; p < end; ++p){
+		if (*p < *min) min = p;}
 	return min;
 }
 
-int findMax(int* arr,const int N) //возвращает максимум в массиве
-{
-	int max = arr[0];
-	for (int i = 0; i < N; ++i)if (arr[i] > max)max = arr[i];
+int* findMax(int* beg,int* end){
+	int* max = beg;
+	for (int* p = beg; p < end; ++p)
+	{if (*p > *max) max = p;}
 	return max;
 }
 
-void findMaxMin(int* arr, int N, int& min, int& max) //нахождение 2 параметров максимума и минимума в массиве
-{for (int i = 0; i < N; ++i)
-{ if (arr[i] < min)min = arr[i];
-  if (arr[i] > max) max = arr[i];}
+void findMaxMin(int* beg, int* end, int& min, int& max)//нахождение 2 параметров максимума и минимума в массиве 
+{  for (int* p = beg; p < end; ++p){
+	if (*p < min)min = *p;
+    if (*p > max)max = *p;}
 }
 
-int findSum(int* arr,const int N) //возвращает сумму элементов в массиве
-{ int sum = 0;
-	for (int i = 0; i < N; i++) sum += arr[i];
+int findSum(int* beg, int* end) //возвращает сумму элементов в массиве
+{    int sum = 0;
+	for (int* p = beg; p < end; ++p) sum += *p;
 	return sum;
 }
 
-void reverse(int* arr, const int N)// reverse "на месте"(Преобразуем текущий массив: записываем значения в "обратном порядке" (начиная от последнего элемента и заканчивая первым))
-{ int buf;
-	for (int i = 0; i < N / 2; ++i) {
-		buf = arr[i];
-		arr[i] = arr[N - i - 1];
-		arr[N - i - 1] = buf;
-	}
-}
-
-void trim(int* arr, const int N, int a, int b)
-//сжатие массива,удаляя из него все элементы,модуль которых находится в интервале[a,b]
-// Освободившиеся в конце массива элементы заполняем нулями.
-{
-	int j = 0;
-	for (int i = 0; i < N; ++i)
-		if (abs(arr[i]) < a || abs(arr[i]) > b)
-		{
-			arr[j] = arr[i]; j++;
-		}
-	for (int i = j; i < N; ++i) arr[i] = 0;
-}
-
-void howManyPositiveNegativeArray(int* arr, const int N)//кол-во отриц./положит. чисел
+void howManyPositiveNegativeArray(int* beg, int* end)//кол-во отриц./положит. чисел
 {
 	int counterPositive(0), counterNegative(0);
-	for (int i = 0; i < N; ++i) {
-		(arr[i] < 0) ? counterNegative++ : counterPositive++;
+	for (int* p=beg;  p < end; ++p) {
+		(*p < 0) ? counterNegative++ : counterPositive++;
 	}
-	cout << "The value of negative numbers is " << counterNegative << '\n';
-	cout << "The value of positive numbers is " << counterPositive << '\n';
 }
 
-void findChainOfElements(int* arr, const int N)
-//нахождение цепочки элементов
+void bubbleSort(int* beg, int* end)
 {
-	int tmp(arr[0]); int len1(0), len2;
-	for (int i = 0; i < N; ++i) {
-		arr[0] = arr[i + 1];
-		++len1;
-		if (arr[0] == tmp) break;
+	for (int j = 0; j < end - beg - 1; ++j)
+		for (int* p = beg; p < end - 1; ++p)
+		{
+			if (*p > * (p + 1)) swap(*p, *(p + 1));
+		}
+}
+	
+void quickSort(int* beg, int* end) {
+	if ((end - beg) <= 1)return;
+	int* i = beg;
+	int* j = end - 1;
+	int half = *(beg + (end - beg) / 2);
+	while (i <= j) {
+		while (*i < half) ++i;
+		while (*j > half) --j;
+		 if (i <= j)
+		 {
+			swap(*i, *j);
+			++i; --j;
+		}
 	}
-	len2 = N - len1;
-	if (len1 > len2)cout << "the max len is " << len1 << '\n';
-	else if (len1 == len2)cout << "len1 = len2 = " << len1 << '\n';
-	else cout << "the max len is " << len2 << '\n';
+	if (beg < j) quickSort(beg, j + 1);
+	if (end > i) quickSort(i, end);
 }
-
-int* giveMemory(int N = 10) {
-	int* arr = new(nothrow) int[N];
-	if (!arr) { cout << "No memory\n"; exit(1); }
-	return arr;
-}
-void freeMemory(int* arr) 
-{ if (!arr) 
-		delete[] arr; 
-	else cout << "Array is empty\n";}
